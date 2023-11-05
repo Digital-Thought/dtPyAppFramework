a9a72a7ee4020387f7fc6588527f8b01d75fb1c3
Matthew <matthew@digital-thought.org>
2023-11-05 15:18:37 +1100

    Implement pytest

--- tests/core_test.py
+++ tests/core_test.py
@@ -1,16 +1,12 @@
 import sys
 import os
 import logging
-import pytest
 
-sys.path.append(os.path.abspath('../src'))
+sys.path.append(os.path.abspath('./src'))
 
 from dtPyAppFramework.paths import ApplicationPaths
 from dtPyAppFramework.secrets_store import SecretsManager
 from dtPyAppFramework import app_logging
 
-
-class CoreTests:
-
-    def test_1(self):
-        assert 1 == 1
+def test_1():
+    pass
