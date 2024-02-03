0fc3afb269f81f01a5f179e80a2bf06b1e2a1a05
Digital-Thought <matthew@digital-thought.org>
2024-02-03 14:53:10 +1100

    Configured project for publishing to pypi.

--- src/dtPyAppFramework/__init__.py
+++ src/dtPyAppFramework/__init__.py
@@ -1,23 +1,11 @@
-# Import version-related information from the "_version" module
-from ._version import _VERSION, _AUTHOR_EMAIL, _AUTHOR, _TITLE
-from ._version import _MAINTAINER, _DESCRIPTION, _DOCS_URL, _PROJECT_URL
+from application import AbstractApp
 
 # Define module-level variables with version-related information
-__version__ = _VERSION
-__maintainer__ = _MAINTAINER
-__author__ = _AUTHOR
-__author_email__ = _AUTHOR_EMAIL
-__description__ = _DESCRIPTION
-__title__ = _TITLE
-__project_url__ = _PROJECT_URL
-__docs_url__ = _DOCS_URL
+with open('version.dat', 'r') as _version:
+    __version__ = _version.read()
+
 
 # Function to retrieve the version
 def version():
     """Returns the version of the module."""
     return __version__
-
-# Function to retrieve the title
-def title():
-    """Returns the title of the module."""
-    return __title__
--- src/dtPyAppFramework/_version.py
+++
@@ -1,8 +0,0 @@
-_VERSION = '0.1'
-_MAINTAINER = 'Matthew @ Digital-Thought'
-_AUTHOR = 'Digital-Thought'
-_AUTHOR_EMAIL = 'matthew@digital-thought.org'
-_DESCRIPTION = 'A Python library for common features in application development.'
-_TITLE = "dtPyAppFramework"
-_PROJECT_URL = ""
-_DOCS_URL = ""
