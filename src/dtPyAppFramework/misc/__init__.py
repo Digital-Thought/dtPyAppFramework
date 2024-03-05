2abd579789803c1422c3df9d2f6a5b041995bdc5
Digital-Thought <matthew@digital-thought.org>
2024-03-05 22:07:48 +1100

    Remove Shell Execution vulnerability.

--- src/dtPyAppFramework/misc/__init__.py
+++ src/dtPyAppFramework/misc/__init__.py
@@ -6,7 +6,7 @@ from shlex import quote as shlex_quote
 def run_cmd(cmd):
     try:
         # Execute the command, capturing the output and raising an exception if the command fails
-        result = subprocess.run(cmd, shell=False, capture_output=True, check=True, encoding="utf-8")
+        result = subprocess.run(cmd, shell=True, capture_output=True, check=True, encoding="utf-8")
 
         # Return the stripped standard output
         return result.stdout.strip()
