97e723b37528f4e865d3ec21347e6f0563434fcd
Digital-Thought <matthew@digital-thought.org>
2024-03-05 21:40:30 +1100

    Correction to application importing at incorrect level.

--- src/dtPyAppFramework/__init__.py
+++ src/dtPyAppFramework/__init__.py
@@ -1,5 +1,9 @@
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 # Define module-level variables with version-related information
-with open('version.dat', 'r') as _version:
+with open(os.path.join(dir_path, 'version.dat'), 'r') as _version:
     __version__ = _version.read()
 
 
