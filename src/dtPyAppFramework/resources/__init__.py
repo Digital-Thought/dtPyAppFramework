a99c53600a58537cd221d35b5a6398093f646e41
Digital-Thought <matthew@digital-thought.org>
2024-08-06 10:28:53 +1000

    Addition of prioritisation to resource paths.

--- src/dtPyAppFramework/resources/__init__.py
+++ src/dtPyAppFramework/resources/__init__.py
@@ -27,6 +27,10 @@ class ResourceManager(object):
         if not self.application_paths:
             self.application_paths = ApplicationPaths()
         self.resource_paths = self.__load_default_paths__()
+        self._sort_paths()
+
+    def _sort_paths(self):
+        self.resource_paths.sort(key=lambda x: x[1])
 
     def __load_default_paths__(self):
         """
@@ -35,18 +39,20 @@ class ResourceManager(object):
         Returns:
             list: List of default resource paths.
         """
-        return [os.path.join(self.application_paths.usr_data_root_path, "resources"),
-                os.path.join(self.application_paths.app_data_root_path, "resources"),
-                os.path.join(os.getcwd(), "resources")]
+        return [(os.path.join(self.application_paths.usr_data_root_path, "resources"), 100),
+                (os.path.join(self.application_paths.app_data_root_path, "resources"), 200),
+                (os.path.join(os.getcwd(), "resources"), 300)]
 
-    def add_resource_path(self, path):
+    def add_resource_path(self, path, priority=10):
         """
         Add a custom resource path.
 
         Args:
             path (str): Path to be added to the resource paths.
+            priority (int): The priority of the path when doing lookups. Lower the number, the higher the priority.
         """
-        self.resource_paths.append(path)
+        self.resource_paths.append((path, priority))
+        self._sort_paths()
 
     def remove_resource_path(self, path):
         """
@@ -55,7 +61,14 @@ class ResourceManager(object):
         Args:
             path (str): Path to be removed from the resource paths.
         """
-        self.resource_paths.remove(path)
+        p_to_remove = None
+        for p in self.resource_paths:
+            if p[0] == path:
+                p_to_remove = p
+                break
+
+        if p_to_remove is not None:
+            self.resource_paths.remove(p_to_remove)
 
     def get_resource_path(self, resource):
         """
@@ -68,9 +81,9 @@ class ResourceManager(object):
             str: Full path to the resource, or None if not found.
         """
         for path in self.resource_paths:
-            if os.path.exists(os.path.join(path, resource)):
-                logging.info(f'Returning resource from: "{os.path.join(path, resource)}"')
-                return os.path.join(path, resource)
+            if os.path.exists(os.path.join(path[0], resource)):
+                logging.info(f'Returning resource from: "{os.path.join(path[0], resource)}"')
+                return os.path.join(path[0], resource)
 
         logging.error(f'Resource: "{resource}" could not be found')
         return None
